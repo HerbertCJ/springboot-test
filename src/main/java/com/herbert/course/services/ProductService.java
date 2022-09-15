c5cfe9b5e991212783ff5e49e386eab89cb113d1
Herbert Cabus Jacby <herbertcj3@gmail.com>
2022-09-15 16:46:04 -0300

    Product + Many-to-many association with JoinTable

---
+++ src/main/java/com/herbert/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.herbert.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.herbert.course.entities.Product;
+import com.herbert.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
