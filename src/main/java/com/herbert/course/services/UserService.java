87e0a50774a4d2162f53a9403177b65b7180eb90
Herbert Cabus Jacby <herbertcj3@gmail.com>
2022-09-15 10:14:13 -0300

    Service layer, component registration

---
+++ src/main/java/com/herbert/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.herbert.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.herbert.course.entities.User;
+import com.herbert.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
